fa8d1fee98314b43d913e52af1910215a7f1a117
PArguelles <pedro.arguelles@gmail.com>
2019-02-26 18:05:11 +0000

    Add files via upload

---
+++ _updates/readGeneticResults.py
@@ -0,0 +1,15 @@
+
+
+combinations = [('rmsd','gdt_2','gdt_4','maxsub','tm']
+
+for m1, m2 in combinations:
+
+
+with open(path_to_file, 'r') as fp:
+
+	line = fp.readline()
+	while line:
+	
+		
+	
+		line = fp.readline()
